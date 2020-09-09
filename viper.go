fca3d8f461420f0e744b30f579551bac4c55cfde
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-09-09 22:35:18 +0200

    Add log for remote config errors
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- viper.go
+++ viper.go
@@ -1795,9 +1795,13 @@ func (v *Viper) getKeyValueConfig() error {
 	for _, rp := range v.remoteProviders {
 		val, err := v.getRemoteConfig(rp)
 		if err != nil {
+			jww.ERROR.Printf("get remote config: %s", err)
+
 			continue
 		}
+
 		v.kvstore = val
+
 		return nil
 	}
 	return RemoteConfigError("No Files Found")
